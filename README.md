d841501c5d10c2fe621265e44dc2f87e864f751c
danielzanata <dzanatha@gmail.com>
2023-12-08 16:46:43 +0000

    adiciona scripts `lint: check e fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Curso.dev - Filipe Deschamps - do Zero ao além
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Se liga ai... Curso.dev é top !</h1>
+  return <h1>Se liga ai... Curso.dev é top !</h1>;
 }
 
 export default Home;
